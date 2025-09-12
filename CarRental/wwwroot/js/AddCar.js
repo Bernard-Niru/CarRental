d0e6a2dddead9ec8860a3ddbb598b0d3506125d0
UT010776 <UT010776@UNICOMTIC39>
2025-09-12 16:26:38 +0530

    DashBoard

---
+++ CarRental/wwwroot/js/AddCar.js
@@ -0,0 +1,4 @@
+﻿document.getElementById("color-select").addEventListener("change", function () {
+    const customColor = document.getElementById("custom-color-input");
+    customColor.style.display = this.value === "Other" ? "block" : "none";
+});
