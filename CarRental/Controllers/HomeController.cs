584a8305ba0e0398be19ffaefbcdf4e96ac34fed
UT010776 <UT010776@UNICOMTIC39>
2025-09-13 15:59:03 +0530

    guest page

--- CarRental/Controllers/HomeController.cs
+++ CarRental/Controllers/HomeController.cs
@@ -1,25 +1,27 @@
 using System.Data;
 using System.Diagnostics;
 using CarRental.Models;
+using CarRental.Services.Implementations;
 using CarRental.Services.Interfaces;
 using CarRental.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CarRental.Controllers
 {//username
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly IBrandService _brandService;
         private readonly ICarService _carService;
         private readonly IUserService _userservice;
 
-        public HomeController(ILogger<HomeController> logger , ICarService carService , IUserService userService)
+        public HomeController(ILogger<HomeController> logger , ICarService carService , IUserService userService, IBrandService brandService)
         {
             _logger = logger;
             _carService = carService;
             _userservice = userService;
-
+            _brandService = brandService;
         }
         [HttpPost]
         public async Task<IActionResult> Register(UserViewModel User)
@@ -76,8 +78,29 @@ namespace CarRental.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            
+            // Get all brands to populate the filter dropdown
+            var brands = _brandService.GetAll()
+                                      .Select(b => new SelectListItem
+                                      {
+                                          Value = b.BrandID.ToString(),
+                                          Text = b.BrandName
+                                      })
+                                      .ToList();
+
+            // Get all available cars
+            var cars = _carService.GetAll();
+
+            // Create a ViewModel to hold both lists and pass it to the view
+            var guestViewModel = new GuestPageViewModel
+            {
+                Cars = cars,
+                BrandOptions = brands
+            };
+
+            return View(guestViewModel);
         }
+    
 
         public IActionResult Privacy()
         {
@@ -89,10 +112,39 @@ namespace CarRental.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult GuestPage()
-        {
-            var Car = _carService.GetAll();
-            return View(Car);
-        }
+
+
+        //public IActionResult GuestPage()
+        //{
+        //    var Car = _carService.GetAll();
+        //    return View(Car);
+        //}
+
+        //public IActionResult GuestPage()
+        //{
+        //    // Get all brands to populate the filter dropdown
+        //    var brands = _brandService.GetAll()
+        //                              .Select(b => new SelectListItem
+        //                              {
+        //                                  Value = b.BrandID.ToString(),
+        //                                  Text = b.BrandName
+        //                              })
+        //                              .ToList();
+
+        //    // Get all available cars
+        //    var cars = _carService.GetAll();
+
+        //    // Create a ViewModel to hold both lists and pass it to the view
+        //    var guestViewModel = new GuestPageViewModel
+        //    {
+        //        Cars = cars,
+        //        BrandOptions = brands
+        //    };
+
+        //    return View(guestViewModel);
+        //}
+
+
     }
+   
 }
---
+++ CarRental/ViewModels/GuestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CarRental.DTOs;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace CarRental.ViewModels
+{
+    public class GuestPageViewModel
+    {
+        public IEnumerable<CarDTO> Cars { get; set; }
+        public IEnumerable<SelectListItem> BrandOptions { get; set; }
+    }
+}
