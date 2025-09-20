b38fc562ff8298974a8e80d1fac005aa2b281dee
UT010776 <UT010776@UNICOMTIC39>
2025-09-20 13:14:06 +0530

    Error Fix

--- CarRental/ViewModels/AdminDashboardViewModel.cs
+++ CarRental/ViewModels/AdminDashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CarRental.ViewModels
+﻿using CarRental.DTOs;
+
+namespace CarRental.ViewModels
 {
     public class AdminDashboardViewModel
     {
@@ -8,6 +10,24 @@
         public int ReturnedBookings { get; set; }
         public int NewUsersThisMonth { get; set; }
         public int ActiveCustomers { get; set; }
+
+
+        // Bookings trend
+        public List<string> Labels { get; set; } = new();
+        public List<int> BookingCounts { get; set; } = new();
+
+        // Top rented cars
+        public List<CarDTO> TopCars { get; set; } = new();
+
+        // Requests breakdown
+        public int AcceptedRequests { get; set; }
+        public int RejectedRequests { get; set; }
+        public int CancelledRequests { get; set; }
+
+        // Revenue
+        public decimal TotalRevenue { get; set; }
+        public List<decimal> MonthlyRevenue { get; set; } = new();
+
     }
 
 }
